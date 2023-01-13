7a89ac3212e3e6590e2bef412e152585bde46755
Issac In <66139562+issac-in@users.noreply.github.com>
2023-01-12 20:58:28 -0800

    Prevent double borders for grid
    Also remove the exploratory for code the hover effect, as it's not
    what I want. I want to the user to only be able to change the colors
    via the effect when their mouse button is down and dragged.

--- logic.js
+++ logic.js
@@ -3,11 +3,6 @@ const gridContainer = document.getElementById("grid");
 for (let i = 0; i < (16**2); i++) {
   const gridItem = document.createElement("div");
   gridItem.classList.add("grid-item");
-  gridItem.setAttribute("onmouseout","changeColor(this)");
   gridItem.setAttribute("data-cy", "grid-item");
   gridContainer.appendChild(gridItem);
-}
-
-function changeColor(x) {
-  x.style.backgroundColor = "yellow";
 }
