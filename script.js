8a13843c6b93d059f025952a8037f3f3f7912853
Issac In <66139562+issac-in@users.noreply.github.com>
2024-04-30 15:23:50 -0700

    Implement dynamic grid density logic
    Before, there was no existing feature to generate the display of the
    etch-a-sketch, which was the whole point of this project. Now, there is.
    
    Side-effects / Unintuitive consequences of this feature:
    - I have not connected this to any user input yet.
    - The MAXIMUM_DENSITY of the grid is 100, otherwise it looks terrible,
      and it gets really slow to load.
    - For deleteGrid() you cannot use a live NodeList or HTMLCollection,
      because the removal of nodes, would mean only half get removed. It was
      a deliberate choice to use document.querySelectorAll(), as it returns
      a static NodeList, unlike something like parentNode.children, which
      was the original plan.
    - For isCornerSquare() the parameter name sqrtDensity was an attempt to
      make it more clear that a sketchpad's width would only consistent of
      the sqrtDensity worth of pixels (e.g. 16). squareIndex is a direct
      reference to how each of the generated pixels has an id assigned based
      on the index of its generation (e.g. 0 to 255).

--- script.js
+++ script.js
@@ -1,1 +1,95 @@
 "use strict"
+
+function checkGridAlreadyExists(parentNode) {
+    return parentNode.children.length;
+}
+
+function checkInvalidGridDensity(num) {
+    const MINIMUM_DENSITY = 16;
+    const MAXIMUM_DENSITY = 100;
+    let invalidGridDensity = true;
+    
+    if (num < MINIMUM_DENSITY || num > MAXIMUM_DENSITY) {
+        console.error(`Expected a density between 16 & 100, got ${num}.`);
+    } else {
+        invalidGridDensity = false;
+    }
+    return invalidGridDensity;
+}
+
+function deleteGrid() {
+    const pixels = document.querySelectorAll(".pixel");
+    for (const pixel of pixels) {
+        pixel.remove();
+    } 
+}
+
+function isCornerSquare(sqrtDensity, squareIndex) {
+    const TOP_LEFT_INDEX = 0;
+    const TOP_RIGHT_INDEX = sqrtDensity - 1;
+    const BOTTOM_LEFT_INDEX = sqrtDensity**2 - sqrtDensity;
+    const BOTTOM_RIGHT_INDEX = sqrtDensity**2 - 1;
+
+    let output;
+    switch (squareIndex) {
+        case TOP_LEFT_INDEX:
+            output = "topLeft";
+            break;
+        case TOP_RIGHT_INDEX:
+            output = "topRight";
+            break;
+        case BOTTOM_LEFT_INDEX:
+            output = "bottomLeft";
+            break;
+        case BOTTOM_RIGHT_INDEX:
+            output = "bottomRight";
+            break;
+        default:
+            output = "not a corner square";
+            break;
+    }
+    return output;
+}
+
+function setGridDensity(num = 16) {
+    if (checkInvalidGridDensity(num)) { return; }
+
+    const sketchPad = document.querySelector(".sketch-pad");
+    const sketchPadWidth = sketchPad.clientWidth;
+    const pixelValue = +(sketchPadWidth / num).toFixed(2);
+    const CORNER_RADIUS = "14px";
+
+    if (checkGridAlreadyExists(sketchPad)) { deleteGrid(); }
+
+    for (let i = 0; i < num**2; i++) {
+        let sketchPadPixel = document.createElement("div");
+        sketchPadPixel.setAttribute("class", "pixel");
+        sketchPadPixel.setAttribute("id", i);
+        sketchPadPixel.setAttribute("style",`
+            width: ${pixelValue}px;
+            height: ${pixelValue}px;
+            background-color: white;
+            border: 1px solid black;
+        `);
+
+        switch (isCornerSquare(num, i)) {
+            case "topLeft":
+                sketchPadPixel.style.borderRadius = `${CORNER_RADIUS} 0 0 0`;
+                break;
+            case "topRight":
+                sketchPadPixel.style.borderRadius = `0 ${CORNER_RADIUS} 0 0`;
+                break;
+            case "bottomLeft":
+                sketchPadPixel.style.borderRadius = `0 0 0 ${CORNER_RADIUS}`;
+                break;
+            case "bottomRight":
+                sketchPadPixel.style.borderRadius = `0 0 ${CORNER_RADIUS} 0`;
+                break;
+        }
+
+        sketchPad.appendChild(sketchPadPixel);
+    }
+}
+
+setGridDensity();
+setGridDensity(32);
